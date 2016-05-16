04f30b5d9f9b98a5b38bcd85957778acf12152db
spasztor <szabolcs1992@gmail.com>
2016-05-15 22:27:34 -0700

    Potentially finished with docstrings for now.

--- source/objects.py
+++ source/objects.py
@@ -13,16 +13,14 @@ import uuid
 import math
 
 def new_handle(handle=None):
-    """ Creates a new handle from an existing one or a new one if None is specified."""
-    if handle is not None:
+    """ Creates a new handle or returns the passed handle if it's valid. """
+    if handle is not None and str(handle) == uuid.UUID(handle, 4):
         return handle
     else:
         return uuid.uuid4()
 
 class Point:
-    """ A basic point object with a northing, easting, elevation and handle.
-        
-    """
+    """ Point class containing a northing, easting, elevation and handle. """
     def __init__(self, northing=0.0, easting=0.0, elevation=0.0, handle=None):
         self.northing = northing
         self.easting = easting
@@ -30,31 +28,32 @@ class Point:
         self.handle = new_handle(handle)
 
 class Line:
+    """ Line class containing a starting point, end point, potential center_point and handle. """
     def __init__(self, start=Point(), end=Point(),
                  center_point=None, handle=None):
         self.start = start
         self.end = end
-        if center_point is not None \
-           and self._is_valid_center_point(center_point):
+        self.center_point = center_point
+        if self._is_valid_center_point():
             self.center_point = center_point
-        else:
-            self.center_point = None
         self.handle = new_handle(handle)
 
-    def _is_valid_center_point(self, center_point):
-        distance_to_start = Line(center_point, self.start).get_length()
-        distance_to_end = Line(center_point, self.end).get_length()
+    def _is_valid_center_point(self):
+        """ Checks to see if the center_point is equadistant to the start and end point. """
+        distance_to_start = Line(self.center_point, self.start).get_length()
+        distance_to_end = Line(self.center_point, self.end).get_length()
         return True if distance_to_start == distance_to_end else False
 
-    def get_length(self, point1=None, point2=None):
-        point1 = self.start
-        point2 = self.end
-        northing_distance = point2.northing - point1.northing
-        easting_distance = point2.easting - point1.easting
-        elevation_distance = point2.elevation - point1.elevation
+    def get_length(self):
+        """ Calculates the length of the line using northing, easting and elevation. """
+        northing_distance = self.end.northing - self.start.northing
+        easting_distance = self.end.easting - self.start.easting
+        elevation_distance = self.end.elevation - self.start.elevation
         return math.sqrt(northing_distance ^ 2 + easting_distance ^ 2 + elevation_distance ^ 2)
 
 class Polyline:
+    """ A polyline object which is a dictionary of lines and a handle.
+    """
     def __init__(self, handle=None, *arg):
         self.elements = {}
         self.handle = new_handle(handle)
