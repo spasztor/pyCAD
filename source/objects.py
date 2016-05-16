e26435683ff1bf834d96401a08c1d87f53a97cb8
Szabolcs <szabolcs1992@gmail.com>
2016-05-15 20:00:08 -0700

    Moved distance function as method to line.

--- source/objects.py
+++ source/objects.py
@@ -8,12 +8,6 @@ def new_handle(arg_handle=False):
     else:
         return uuid.uuid4()
 
-def distance_between_points(point1, point2):
-    northing_distance = point2.northing - point1.northing
-    easting_distance = point2.easting - point1.easting
-    elevation_distance = point2.elevation - point1.elevation
-    return math.sqrt(northing_distance ^ 2 + easting_distance ^ 2 + elevation_distance ^ 2)
-
 class Point:
     def __init__(self, northing=0.0, easting=0.0, elevation=0.0, handle=False):
         self.northing = northing
@@ -22,21 +16,27 @@ class Point:
         self.handle = new_handle(handle)
 
 class Line:
-    def __init__(self, arg_point1=Point(), arg_point2=Point(),
-                 arg_center_point=False, arg_handle=False):
-        self.point1 = arg_point1
-        self.point2 = arg_point2
-        if not arg_center_point \
-           and self._is_valid_center_point(arg_center_point):
-            self.center_point = arg_center_point
+    def __init__(self, start=Point(), end=Point(),
+                 center_point=False, handle=False):
+        self.start = start
+        self.end = end
+        if not center_point \
+           and self._is_valid_center_point(center_point):
+            self.center_point = center_point
         else:
             self.center_point = None
-        self.handle = new_handle(arg_handle)
+        self.handle = new_handle(handle)
 
-    def _is_valid_center_point(arg_center_point):
-        distance1 = distance_between_points(arg_center_point, self.point1)
-        distance2 = distance_between_points(arg_center_point, self.point2)
-        return True if distance1 == distance2 else False
+    def _is_valid_center_point(center_point):
+        distance_to_start = Line(center_point, self.start).get_length()
+        distance_to_end = Line(center_point, self.end).get_length()
+        return True if distance_to_start == distance_to_end else False
+
+    def get_length(point1=self.start, point2=self.end)
+        northing_distance = point2.northing - point1.northing
+        easting_distance = point2.easting - point1.easting
+        elevation_distance = point2.elevation - point1.elevation
+        return math.sqrt(northing_distance ^ 2 + easting_distance ^ 2 + elevation_distance ^ 2)
 
 class Polyline:
     def __init__(self, arg_handle=False, *arg):
